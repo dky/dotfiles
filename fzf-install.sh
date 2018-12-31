496067c3afca68ec696a27f4a338406fc88e035f
Don Ky <don.d.ky@gmail.com>
2018-12-31 14:29:46 -0500

    Tweak installer

--- fzf-install.sh
+++ fzf-install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+FZF_DIR=~/.fzf
+
+if [ -d $FZF_DIR ]; then
+	echo "Directory $FZF_DIR exists."
+	rm -rf $FZF_DIR
+	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+	$FZF_DIR/install
+fi
