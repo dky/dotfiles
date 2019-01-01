682aed8aa1277bbc9fb120def66ada7189dcf72a
Don Ky <don.d.ky@gmail.com>
2019-01-01 15:52:03 +0000

    Install if .fzf doesn't exist

--- fzf-install.sh
+++ fzf-install.sh
@@ -2,7 +2,7 @@
 
 FZF_DIR=~/.fzf
 
-if [ -d $FZF_DIR ]; then
+if [ ! -d $FZF_DIR ]; then
 	echo "Directory $FZF_DIR exists."
 	rm -rf $FZF_DIR
 	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
