74cab4dbed950b7eef6a9dcc62da00a61975fe82
Don Ky <don.d.ky@gmail.com>
2019-03-23 12:45:40 +0000

    nvim-bootstrap

---
+++ nvim-bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+nvim -c PlugInstall
+#nvim -c GoInstallBinaries
+#nvim -c UpdateRemotePlugins
+#nvim -c checkHealth
+
+
+
+#go get -u github.com/mdempsky/gocode
+
+#Python autocompletion
+pip3 install jedi
