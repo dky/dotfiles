25d2f02045b482c729562bef3305398446551e07
Don Ky <don.d.ky@gmail.com>
2020-04-12 17:34:01 -0400

    Add keybindings.bash, update comments

--- .fzf.bash
+++ .fzf.bash
@@ -8,12 +8,13 @@ fi
 # ---------------
 [[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
-# Key bindings
+# Key bindings, Load customized keybindings from dotfiles
 # ------------
-source "$HOME/.fzf/shell/key-bindings.bash"
-
+source "$HOME/.dotfiles/fzf-key-bindings.bash"
 
 #determines search program for fzf
+#This is only called if you use fzf command directly, won't be called with ctrl-t
+#Overrides for ctrl-t are in the fzf-key-bindings.bash above to exclude search dirs.
 #ag = silver_searcher/rg = ripgrep
 if type ag &> /dev/null; then
 	export FZF_DEFAULT_COMMAND='ag -p ~/.gitignore -g ""'
