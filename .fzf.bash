07c61cd47ff24a3d8a4de74aa79dbb58b1de9ca5
Don Ky <don.d.ky@gmail.com>
2020-04-12 22:03:23 -0400

    Clean up stray .fzf files and further comment

--- .fzf.bash
+++ .fzf.bash
@@ -1,25 +1,22 @@
 # Setup fzf
-# ---------
 if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
 	export PATH="$PATH:$HOME/.fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
+# Auto-completion, use the default from fzf installation
 [[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
-# Key bindings, Load customized keybindings from dotfiles
-# ------------
-source "$HOME/.dotfiles/fzf-key-bindings.bash"
+# Key bindings, Load customized keybindings from dotfiles with excluded directories
+# Customizations are handled in .dotfiles/.fzf/key-bindings.bash
+source "$HOME/.dotfiles/.fzf/key-bindings.bash"
 
-#determines search program for fzf
-#This is only called if you use fzf command directly, won't be called with ctrl-t
-#Overrides for ctrl-t are in the fzf-key-bindings.bash above to exclude search dirs.
-#ag = silver_searcher/rg = ripgrep
+# Which search program to use for fzf
+# This is only used if you use fzf command from shell, won't be called with ctrl-t
+# ag = silver_searcher
+# rg = ripgrep
 if type ag &> /dev/null; then
 	export FZF_DEFAULT_COMMAND='ag -p ~/.gitignore -g ""'
 fi
-#refer rg over ag
 if type rg &> /dev/null; then
 	export FZF_DEFAULT_COMMAND='rg --files --hidden'
 fi
