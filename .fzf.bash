128eccb0abfcc5646e28297089256ed4d72bb2bc
Don Ky <don.d.ky@gmail.com>
2020-04-16 07:26:53 -0400

    preview below

--- .fzf.bash
+++ .fzf.bash
@@ -13,6 +13,6 @@ source "$HOME/.dotfiles/.fzf/key-bindings.bash"
 # Which search program to use for fzf
 # This is only used if you use fzf command from shell, won't be called with ctrl-t
 # rg = ripgrep
-export FZF_DEFAULT_OPTS='--height=70% --preview="cat {}" --preview-window=right:60%:wrap'
+export FZF_DEFAULT_OPTS='--height=90% --preview="cat {}" --preview-window=down:60%:wrap'
 export FZF_DEFAULT_COMMAND='rg --files --hidden'
 export FZF_CTRL_T_COMMAND='$FZF_DEFAULT_COMMAND'
