82c22732e3007db3c27270ab84d6e02d95341e21
Don Ky <don.d.ky@gmail.com>
2020-04-16 09:55:20 -0400

    Don't be dependent on rg

--- .fzf.bash
+++ .fzf.bash
@@ -14,5 +14,3 @@ source "$HOME/.dotfiles/.fzf/key-bindings.bash"
 # This is only used if you use fzf command from shell, won't be called with ctrl-t
 # rg = ripgrep
 export FZF_DEFAULT_OPTS='--height=90% --preview="cat {}" --preview-window=down:60%:wrap'
-export FZF_DEFAULT_COMMAND='rg --files --hidden'
-export FZF_CTRL_T_COMMAND='$FZF_DEFAULT_COMMAND'
