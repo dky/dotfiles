7365db092141d4b214756b261407a5ecda36d2f7
Don Ky <don.d.ky@gmail.com>
2021-01-04 19:29:06 -0500

    Add comment + check if ranger dir exists prior to pulling icons

--- misc.sh
+++ misc.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+# This script is to bootstrap Coc post nvim coc plugin install. It's mean't to suppliment anything we can't do after running plug install.
 
 # Ranger devicons
-#git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+if [ -d "~/.config/ranger/plugins/ranger_devicons" ] 
+then
+    echo "Ranger devicons exist, doing nothing" 
+else
+	echo "Cloning ranger devicons"
+	git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+fi
 
 # Coc
 mkdir -p ~/.config/coc/extensions
