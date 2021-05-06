e68966b55fe96dafa37d441e83075d1fd70c1ce9
Don Ky <dky@ted.com>
2021-05-06 16:12:48 -0400

    Remove coc-pairs

--- misc.sh
+++ misc.sh
@@ -13,4 +13,4 @@ fi
 # Coc
 mkdir -p ~/.config/coc/extensions
 cd ~/.config/coc/extensions
-npm install coc-css coc-dictionary coc-eslint coc-go coc-html coc-json coc-neosnippet coc-pairs coc-prettier coc-pyright coc-python coc-tsserver coc-vetur coc-yaml --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+npm install coc-css coc-dictionary coc-eslint coc-go coc-html coc-json coc-neosnippet coc-prettier coc-pyright coc-python coc-tsserver coc-vetur coc-yaml --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
