b4324372982442d8b01fd2bbe06538d5e4ea352c
Don Ky <don.d.ky@gmail.com>
2021-06-20 15:40:05 -0400

    Add some checks for on Darwin or Linux + check if coc extensions are installed

--- misc.sh
+++ misc.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # This script is to bootstrap Coc post nvim coc plugin install. It's mean't to suppliment anything we can't do after running plug install.
 
-# Brew install bat for syntax highlighting with fzf
-brew install bat
+if [ "$(uname)" == "Darwin" ]; then
+	# Brew install bat for syntax highlighting with fzf
+	brew install bat
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	echo "On Linux, installing nodejs, npm because this is required for Coc to work"
+	sudo apt-get install nodejs npm
+fi
 
 # Ranger devicons
 if [ -d "~/.config/ranger/plugins/ranger_devicons" ] 
@@ -13,7 +18,9 @@ else
 	git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
 fi
 
-# Coc
+# Install Coc - Switch to native LSP soon so we don't have this bloat
+if [ ! -d "~/.config/coc/extensions" ]; then
 mkdir -p ~/.config/coc/extensions
 cd ~/.config/coc/extensions
 npm install coc-solargraph coc-css coc-dictionary coc-eslint coc-go coc-html coc-json coc-neosnippet coc-prettier coc-pyright coc-python coc-tsserver coc-vetur coc-yaml --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+fi
