634909fe0f319d91ddd725500ca3261d13ffa0a6
Don Ky <don.d.ky@gmail.com>
2021-07-05 16:19:39 -0400

    Add vim diagnostics

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -46,3 +46,12 @@ require'nvim-treesitter.configs'.setup {
 		enable = true
 	},
 }
+
+-- Vim diagnostics, more to do here
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+ vim.lsp.diagnostic.on_publish_diagnostics, {
+   virtual_text = true,
+	signs = true,
+	update_in_insert = false,
+ }
+)
