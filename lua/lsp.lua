28f853e187e18043c5576bf4c6698c36d7371300
Don Ky <don.d.ky@gmail.com>
2021-07-05 16:44:57 -0400

    Disable virtual text for errors/warnings

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -50,7 +50,7 @@ require'nvim-treesitter.configs'.setup {
 -- Vim diagnostics, more to do here
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
  vim.lsp.diagnostic.on_publish_diagnostics, {
-   virtual_text = true,
+   virtual_text = false,
 	signs = true,
 	update_in_insert = false,
  }
