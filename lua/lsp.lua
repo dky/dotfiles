8e9f164d5ac02589800e0f7570f0def2f80509ab
Don Ky <don.d.ky@gmail.com>
2021-07-11 15:53:32 -0400

    Fix up indenting

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -49,89 +49,89 @@ require'nvim-treesitter.configs'.setup {
 
 -- Setup diagnostics formaters and linters for non LSP provided files
 nvim_lsp.diagnosticls.setup {
-    on_attach = on_attach,
-    --capabilities = lsp_status.capabilities,
-    cmd = {"diagnostic-languageserver", "--stdio"},
-    filetypes = {
-        "lua",
-        "sh",
-        "markdown",
-        "json",
-        "yaml",
-        "toml"
-    },
-    init_options = {
-        linters = {
-            shellcheck = {
-                command = "shellcheck",
-                debounce = 100,
-                args = {"--format", "json", "-"},
-                sourceName = "shellcheck",
-                parseJson = {
-                    line = "line",
-                    column = "column",
-                    endLine = "endLine",
-                    endColumn = "endColumn",
-                    message = "${message} [${code}]",
-                    security = "level"
-                },
-                securities = {
-                    error = "error",
-                    warning = "warning",
-                    info = "info",
-                    style = "hint"
-                }
-            },
-            markdownlint = {
-                command = "markdownlint",
-                isStderr = true,
-                debounce = 100,
-                args = {"--stdin"},
-                offsetLine = 0,
-                offsetColumn = 0,
-                sourceName = "markdownlint",
-                formatLines = 1,
-                formatPattern = {
-                    "^.*?:\\s?(\\d+)(:(\\d+)?)?\\s(MD\\d{3}\\/[A-Za-z0-9-/]+)\\s(.*)$",
-                    {
-                        line = 1,
-                        column = 3,
-                        message = {4}
-                    }
-                }
-            }
-        },
-        filetypes = {
-            sh = "shellcheck",
-            markdown = "markdownlint"
-        },
-        formatters = {
-            shfmt = {
-                command = "shfmt",
-                args = {"-i", "2", "-bn", "-ci", "-sr"}
-            },
-            prettier = {
-                command = "prettier",
-                args = {"--stdin-filepath", "%filepath"},
-            }
-        },
-        formatFiletypes = {
-            sh = "shfmt",
-            json = "prettier",
-            yaml = "prettier",
-            toml = "prettier",
-            markdown = "prettier",
-            lua = "prettier"
-        }
-    }
+	on_attach = on_attach,
+	--capabilities = lsp_status.capabilities,
+	cmd = {"diagnostic-languageserver", "--stdio"},
+	filetypes = {
+		"lua",
+		"sh",
+		"markdown",
+		"json",
+		"yaml",
+		"toml"
+	},
+	init_options = {
+		linters = {
+			shellcheck = {
+				command = "shellcheck",
+				debounce = 100,
+				args = {"--format", "json", "-"},
+				sourceName = "shellcheck",
+				parseJson = {
+					line = "line",
+					column = "column",
+					endLine = "endLine",
+					endColumn = "endColumn",
+					message = "${message} [${code}]",
+					security = "level"
+				},
+				securities = {
+					error = "error",
+					warning = "warning",
+					info = "info",
+					style = "hint"
+				}
+			},
+			markdownlint = {
+				command = "markdownlint",
+				isStderr = true,
+				debounce = 100,
+				args = {"--stdin"},
+				offsetLine = 0,
+				offsetColumn = 0,
+				sourceName = "markdownlint",
+				formatLines = 1,
+				formatPattern = {
+					"^.*?:\\s?(\\d+)(:(\\d+)?)?\\s(MD\\d{3}\\/[A-Za-z0-9-/]+)\\s(.*)$",
+					{
+						line = 1,
+						column = 3,
+						message = {4}
+					}
+				}
+			}
+		},
+		filetypes = {
+			sh = "shellcheck",
+			markdown = "markdownlint"
+		},
+		formatters = {
+			shfmt = {
+				command = "shfmt",
+				args = {"-i", "2", "-bn", "-ci", "-sr"}
+			},
+			prettier = {
+				command = "prettier",
+				args = {"--stdin-filepath", "%filepath"},
+			}
+		},
+		formatFiletypes = {
+			sh = "shfmt",
+			json = "prettier",
+			yaml = "prettier",
+			toml = "prettier",
+			markdown = "prettier",
+			lua = "prettier"
+		}
+	}
 }
 
 
 -- Vim diagnostics, more to do here
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
- vim.lsp.diagnostic.on_publish_diagnostics, {
-   virtual_text = false,
+vim.lsp.diagnostic.on_publish_diagnostics, {
+	virtual_text = false,
 	signs = true,
 	update_in_insert = false,
- }
+}
 )
