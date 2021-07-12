e77a96d612ca3e7848a6c500310c9bac91316860
Don Ky <don.d.ky@gmail.com>
2021-07-11 20:51:54 -0400

    Add comment for ranbow stanza

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -6,7 +6,7 @@ require'nvim-treesitter.configs'.setup {
 		enable = true, -- false will disable the whole extension
 	},
 	rainbow = {
-		enable = true,
+		enable = true, -- This is used by the nvim-ts-rainbow plugin.
 		extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
 		max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
 	}
