4816691b2f0dac37b682b86eb2f8a1b00f054ab2
Don Ky <don.d.ky@gmail.com>
2021-07-11 20:34:17 -0400

    Fmt treesitter

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,17 +1,17 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {
-      'bash', 'go', 'python', 'lua', 'comment', 'json', 'regex', 'yaml', 'rust'
-  },
-  highlight = {
-    enable = true,              -- false will disable the whole extension
-  },
+	ensure_installed = {
+		'bash', 'go', 'python', 'lua', 'comment', 'json', 'regex', 'yaml', 'rust'
+	},
+	highlight = {
+		enable = true, -- false will disable the whole extension
+	},
 }
 
 -- nvim-ts-rainbow
 require'nvim-treesitter.configs'.setup {
-  rainbow = {
-    enable = true,
-    extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-    max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-  }
+	rainbow = {
+		enable = true,
+		extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+		max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+	}
 }
