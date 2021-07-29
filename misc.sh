c8d3794545c1fbec062766b56efd95ce05196d5e
Don Ky <don.d.ky@gmail.com>
2021-07-29 00:04:54 -0400

    Setup additional bootstrapping for nvim

--- misc.sh
+++ misc.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
-# This script is to bootstrap Coc post nvim coc plugin install. It's mean't to suppliment anything we can't do after running plug install.
+# This script is to bootstrap nvim
+
+# Setup plug for nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 if [ "$(uname)" == "Darwin" ]; then
-	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
 	# Brew install bat for syntax highlighting with fzf
 	brew install bat
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	echo "On Linux, installing nodejs, npm because this is required for Coc to work"
-	sudo apt-get -y install nodejs npm
+	# These packages *should* be handled by terraform but just in case.
+	sudo apt-get -y install bats gcc g++ nodejs npm
 	# Get latest fzf
 	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 	y | ~/.fzf/install
 	# Undo any modifications the .fzf.bash file
 	git checkout ~/.dotfiles/.fzf.bash
+
+	# setup lua custom dictionary
+	lua_custom_dictionary_script="$HOME/.config/nvim/lua/compe-custom-dictionary.lua"
+	if [ -f $lua_custom_dictionary_script ]
+	then
+		rm $lua_custom_dictionary_script
+		cd $HOME/.config/nvim/lua && ln -s $lua_custom_dictionary_script .
+	fi
+
+	mkdir $HOME/.vim-dictionary && touch $HOME/.vim-dictionary/custom_dictionary.txt
+
+	mkdir $HOME/bin
+	wget https://github.com/neovim/neovim/releases/download/v0.5.0/nvim.appimage -O $HOME/bin/nvim && chmod u+x $HOME/bin/nvim
 fi
 
 # Ranger devicons
