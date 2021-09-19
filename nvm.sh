77dcdacf30912ff6d46e4c6df33f226d91fb8dbf
Don Ky <don.d.ky@gmail.com>
2021-09-19 12:59:59 -0400

    Add nvm.sh script

---
+++ nvm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Node version manager - Bootstrap node using nvm
+
+# Install it, you should have the aliases already defined
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+# Install latest version of node
+nvm install node
+
+# Install all the lsp cruft
+npm -g install pyright
+npm -g install bash-language-server
+npm -g install dockerfile-language-server-nodejs
+npm -g install yaml-language-server
+npm -g install yarn
