56b03e1142d4541fda5783dd840fcedf714ac4f3
Don Ky <don.d.ky@gmail.com>
2021-10-31 14:16:06 -0400

    Remove source

--- node.sh
+++ node.sh
@@ -4,7 +4,6 @@
 # Install it, you should have the aliases already defined
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
-#source bashrc
 source ~/.bashrc
 
 # Install latest version of node
