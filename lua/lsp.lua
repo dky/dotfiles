3207087436845a64d7bd03545e39fda329a0b4a2
Don Ky <don.d.ky@gmail.com>
2021-11-03 18:54:46 -0400

    Disable <space>f mapping which is conflicting with whichkey leader

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -29,11 +29,13 @@ local on_attach = function(client, bufnr)
     buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
 
     -- Set some keybinds conditional on server capabilities
-    if client.resolved_capabilities.document_formatting then
-        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-    elseif client.resolved_capabilities.document_range_formatting then
-        buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-    end
+    -- if client.resolved_capabilities.document_formatting then
+		  -- Disable because this was conflicting with my leader-f whichkey mapping.
+        -- buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    --elseif client.resolved_capabilities.document_range_formatting then
+		  -- Disable because this was conflicting with my leader-f whichkey mapping.
+        -- buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    --end
 end
 
 local servers = {"pyright", "rust_analyzer", "solargraph", "bashls", "dockerls", "cmake", "clangd"}
