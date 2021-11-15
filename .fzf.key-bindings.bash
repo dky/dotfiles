f7b0ce0a40c48003a880e0916723193598a74e13
Don Ky <don.d.ky@gmail.com>
2021-11-14 19:55:16 -0500

    Simplify the find command

--- .fzf.key-bindings.bash
+++ .fzf.key-bindings.bash
@@ -63,9 +63,7 @@ __fzf_history__() {
 
 __fzf_alias__() {
   local cmd dir
-  cmd="${FZF_ALT_C_COMMAND:-"command find -L . -mindepth 1 \
-    \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
-    -o -type d -print 2> /dev/null | cut -b3-"}"
+  cmd="${FZF_ALT_C_COMMAND:-"command find . -type d -print 2> /dev/null | cut -b3-"}"
   dir=$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS" $(__fzfcmd) +m) && printf 'cd %q' "$dir"
 }
 
