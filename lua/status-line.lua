2ff55660631894c68d54e49c6b55524397a7a688
Don Ky <don.d.ky@gmail.com>
2022-05-08 12:40:09 -0400

    Re-add basic color support

--- lua/status-line.lua
+++ lua/status-line.lua
@@ -1,10 +1,28 @@
 local gl = require('galaxyline')
---local colors = require('galaxyline.theme').default
 local colors = require("galaxyline.highlighting")
-
 local condition = require('galaxyline.condition')
 local gls = gl.section
 gl.short_line_list = {'NvimTree','vista','dbui','packer'}
+
+local colors = {
+    bg = '#282c34',
+    fg = '#aab2bf',
+    section_bg = '#38393f',
+    blue = '#61afef',
+    green = '#98c379',
+    purple = '#c678dd',
+    orange = '#e5c07b',
+    red1 = '#e06c75',
+    red2 = '#be5046',
+    yellow = '#e5c07b',
+    gray1 = '#5c6370',
+    gray2 = '#2c323d',
+    gray3 = '#3e4452',
+    darkgrey = '#5c6370',
+    grey = '#848586',
+    middlegrey = '#8791A5'
+}
+
 
 local function trailing_whitespace()
     local trail = vim.fn.search("\\s$", "nw")
