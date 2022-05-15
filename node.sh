839d1508f08540fed4af63139d25a047beb225d1
Don Ky <don.d.ky@gmail.com>
2022-05-15 17:45:17 -0400

    Bump node nvm to 0.39.1 revert back to i -g for installing packages

--- node.sh
+++ node.sh
@@ -2,7 +2,7 @@
 # Node version manager - Bootstrap node using nvm
 
 # Install it, you should have the aliases already defined
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 source ~/.bashrc
 
@@ -10,10 +10,10 @@ source ~/.bashrc
 nvm install node
 
 # Install all the lsp cruft
-npm install pyright
-npm install bash-language-server
-npm install dockerfile-language-server-nodejs
-npm install yaml-language-server
-npm install yarn
-npm install ansible-language-server
-npm install diagnostic-languageserver
+npm i -g pyright
+npm i -g bash-language-server
+npm i -g dockerfile-language-server-nodejs
+npm i -g yaml-language-server
+npm i -g yarn
+npm i -g ansible-language-server
+npm i -g diagnostic-languageserver
