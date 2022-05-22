d83c2a6bbc7514ef5c6339cf47d216cd914b9422
Don Ky <don.d.ky@gmail.com>
2022-05-22 12:07:32 -0400

    Rust analyzer snippet

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -47,6 +47,30 @@ for _, lsp in ipairs(servers) do
     }
 end
 
+nvim_lsp.rust_analyzer.setup{
+  on_attach = on_attach,
+  settings = {
+    ["rust-analyzer"] = {
+      assist = {
+        importMergeBehavior = "last",
+        importPrefix = "by_self",
+      },
+      diagnostics = {
+        disabled = { "unresolved-import" }
+      },
+      cargo = {
+          loadOutDirsFromCheck = true
+      },
+      procMacro = {
+          enable = true
+      },
+      checkOnSave = {
+          command = "clippy"
+      },
+    }
+  }
+}
+
 -- If you open a ruby file and solargraph does not attach see:
 -- Solargraph https://github.com/neovim/nvim-lspconfig/issues/387
 nvim_lsp.solargraph.setup{}
