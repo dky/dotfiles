cb4759178d666a3619b68c7c29c93092a9b362e1
Don Ky <don.d.ky@gmail.com>
2022-06-18 12:19:24 -0400

    Fix up busted gutter signs

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -218,7 +218,13 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
     vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics,
     {
-        virtual_text = true,
-        update_in_insert = false
+        virtual_text = false,
+        update_in_insert = true
     }
 )
+
+local signs = {Error = " ", Warn = " ", Hint = " ", Info = " "}
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
+end
