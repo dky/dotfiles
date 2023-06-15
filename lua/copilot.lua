062d46911f342161898e300553b888ea6e3c6c1d
Don Ky <don.d.ky@gmail.com>
2023-06-15 08:31:35 -0400

    Ignore dockerfiles

--- lua/copilot.lua
+++ lua/copilot.lua
@@ -36,6 +36,7 @@ require('copilot').setup({
     hgcommit = false,
     svn = false,
     cvs = false,
+    Dockerfile = false,
     ["."] = false,
   },
   copilot_node_command = 'node', -- Node.js version must be > 16.x
