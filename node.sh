ecbfdc4826655d20f857b7452c2292ace73cf886
Don Ky <don.d.ky@gmail.com>
2025-04-16 17:21:50 -0400

    Nuke cruft we might not use

--- node.sh
+++ node.sh
@@ -6,15 +6,15 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
 
 source ~/.bashrc
 
-# Install 18.x version of node
+# Install node
 nvm install 23.11.0
 
 # Install all the lsp cruft
-npm i -g pyright
+#npm i -g pyright
 npm i -g bash-language-server
-npm i -g dockerfile-language-server-nodejs
-npm i -g yaml-language-server
-npm i -g yarn
-npm i -g ansible-language-server
-npm i -g diagnostic-languageserver
-npm i -g vscode-langservers-extracted
+#npm i -g dockerfile-language-server-nodejs
+#npm i -g yaml-language-server
+#npm i -g yarn
+#npm i -g ansible-language-server
+#npm i -g diagnostic-languageserver
+#npm i -g vscode-langservers-extracted
