c687e7bc23bd7b4b45f6be14c49df2bd92fb5cba
Don Ky <don.d.ky@gmail.com>
2025-12-04 18:16:08 +0000

    Pyenv deps

---
+++ pyenv.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Python deps for pyenv
+sudo apt-get update
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
