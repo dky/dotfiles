24e62f9ea9c8d85be879ce33fae5575e5d9a403c
dky <dky@users.noreply.github.com>
2025-12-23 11:40:14 -0500

    Fix treesitter errors

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,13 +1,12 @@
-require'nvim-treesitter.configs'.setup {
-	ensure_installed = {
-		'bash', 'go', 'python', 'lua', 'comment', 'json', 'regex', 'yaml', 'rust', 'hcl'
-	},
-	highlight = {
-		enable = true, -- false will disable the whole extension
-	},
-	rainbow = {
-		enable = true, -- This is used by the nvim-ts-rainbow plugin.
-		extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-		max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-	}
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  config = function()
+    require'nvim-treesitter.configs'.setup {
+      ensure_installed = {
+        'bash', 'go', 'python', 'lua', 'comment', 'json', 'regex', 'yaml', 'rust', 'hcl'
+      },
+      highlight = { enable = true },
+    }
+  end
 }
