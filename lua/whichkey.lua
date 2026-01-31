1879eb4cd888c2ce839fcbdad311e6d67a763f45
dky <dky@users.noreply.github.com>
2026-01-31 10:59:01 -0500

    Clean-up unused mapings

--- lua/whichkey.lua
+++ lua/whichkey.lua
@@ -14,19 +14,12 @@ wk.add({
 
   -- Leader mappings
   { "<leader>f", "<cmd>Files<cr>", desc = "Search all files in current Git Repo" },
-  { "<leader>e", vim.cmd.Ex, desc = "Open file explorer" },
   { "<leader>p", "\"_dP", desc = "Paste without overwrite" },
-  { "<leader>/", "<Plug>(comment_toggle_linewise_current)", desc = "Toggle comment" },
   { "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], desc = "Search and replace word under cursor" },
-  { "<leader>t", ":Today<CR>", desc = "Open today's note" },
   { "<leader><leader>", "<cmd>FloatermToggle<cr>", desc = "Toggle floatterm" },
 
   -- Non-leader mappings
-  { "J", "mzJ`z", desc = "Join lines and keep cursor position" },
-  { "<C-d>", "<C-d>zz", desc = "Half page down and center" },
-  { "<C-u>", "<C-u>zz", desc = "Half page up and center" },
   { "n", "nzzzv", desc = "Next search result and center" },
   { "N", "Nzzzv", desc = "Previous search result and center" },
-  { "Q", "<nop>", desc = "Disable Ex mode" },
 })
 
